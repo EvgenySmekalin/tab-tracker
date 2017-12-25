6c82bf6acb2b5520eb15c0c1b4585345940e5c45
EvgenySmekalin <bakaExpire@mail.ru>
2017-12-25 11:22:42 +0700

    Add sqlite

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,31 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
+const {sequelize} = require('./models')
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const config = require('./config/config')
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! Your user was registered!`
+require('./routes')(app)
+
+io.on('connection', function (socket) {
+  console.log('a user connected')
+  socket.on('disconnect', function () {
+    console.log('user disconnected')
   })
 })
 
+sequelize.sync()
+  .then(() => {
+    http.listen(config.port, function () {
+      console.log(`listening on *:${config.port}`)
+    })
+  })
+
+/*
 app.listen(process.env.PORT || 8081)
+*/
